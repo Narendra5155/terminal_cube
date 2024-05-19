9e0217284436e83fc560591634e58a8c7aabc075
Narendra Hindwar <hindwarnarendra@gmail.com>
2024-05-19 11:27:21 +0530

    Added comment for readability,refactored the code and solved the echoproblem

--- main.c
+++ main.c
@@ -4,8 +4,10 @@
 #include<math.h>
 #include<time.h>
 #include<wchar.h>
-typedef struct Buffer Buffer;
 
+//Define the buffer structure for the 3D object
+
+typedef struct Buffer Buffer;
 struct Buffer{
     wchar_t *buffer;
     float *zbuffer;
@@ -13,69 +15,124 @@ struct Buffer{
     int h;
 };
 
-HWND hWnd;
-double deltatime=0;
-const wchar_t initial=L'.';
-const int L_len=9;
-float horizontal_offset=0;
-float vertical_offset=0;
+
+const float radtoDEG=180/3.14159265359;      //Defining The value of pi and 2pi for degree calculations
+const  float Twopi=6.28318530718;
+
+HWND hWnd;                                  // Handle Variable for the current window
+
+double deltatime=0;                         // Delta Time Variable
+
+const wchar_t initial=L'.';                  // Background Shade
+
+
+const int L_len=9;                          //Length of the illumination and illumination gradient
 const wchar_t map[]=L" ▁▂▃▄▅▆▇█";
-float ldirection[3]={0,0.7071,-0.7071};
-float rotInc[3]={2,2,.4};
-float A=2;
-float B=2;
-float C=2;
-float sa,sb,sc,ca,cb,cc;
-float distance=200;
-float K1=100;
+
+float ldirection[3]={0,0.7071,-0.7071};     // Illumination Vector
+
+float horizontal_offset=0;                  // offset for the screen buffer
+float vertical_offset=0;
+
+
+
+float rotInc[3]={2,2,.4};                   //Rotation Increment for the three axis roation
+
+float A=0;                                  //Defining the rotation angle variable
+float B=0;
+float C=0;
+
+float sa,sb,sc,ca,cb,cc;                    //Variable to precomute sine and cosine
+
+
+float distance=200;                         //Distance form Camera and the screen distance from  
+float K1=100;                               //the object change this to change depth perception
+
+
+
+//X axis Rotation Calculation function(Matrix multipication)
+
 float calcX(float x,float y,float z){
     return cc * cb * x + cc * sb * sa * y - cc * sb * ca * z + 
            sc * ca * y + sc * sa * z;
 }
+
+
+//Y axis Rotation Calculation function(Matrix multipication)
+
 float calcY(float x,float y,float z){
     return cc * ca * y + cc * sa * z - sc * cb * x - 
            sc * sb * sa * y + 
            sc * sb * ca * z ;
 }
+
+//Z axis Rotation Calculation function(Matrix multipication)
+
 float calcZ(float x,float y,float z){
     return sb * x - cb * sa * y + cb * ca * z;
 }
+
+
 void calculateRotation(float x,float y,float z,Buffer *buff,char ch,int *nor){
+    
     float xl=calcX(x,y,z); 
     float yl=calcY(x,y,z);
     float zl=calcZ(x,y,z);
+    
+    //Calculate the Luminance value
+
     float L=(ldirection[0]*calcX(nor[0],nor[1],nor[2]) +
                 ldirection[1]*calcY(nor[0],nor[1],nor[2]) +
                 ldirection[2]*calcZ(nor[0],nor[1],nor[2]));
+    
     zl=zl+distance;
+    
     float iz=1/zl;
+    
     int xp=buff->w/2+(xl*K1*iz*2);
     int yp=buff->h/2-(yl*K1*iz);
+    
     int idx=(yp*buff->w)+xp;
     int aidx;
     if(L>0)
         aidx=L_len*L;
     else
         aidx=0;
+    
     if(idx>=0 && idx<(buff->w*buff->h) && buff->zbuffer[idx]<iz){
         buff->buffer[idx]=map[aidx];
         buff->zbuffer[idx]=iz;
     }
 }
+
+
+//Buffer Initialization
+
 void initBuffer(Buffer *buff){
     int size=buff->w*buff->h;
+    
     buff->buffer=(wchar_t*)malloc(size*sizeof(wchar_t));
     buff->zbuffer=(float*)malloc(size*sizeof(float));
 }
+
+
+//Reseting the buffer to the Background shade
+
 int resetBuffer(Buffer *buff){
     int size=buff->w*buff->h;
     wmemset(buff->buffer,initial,size);
     memset(buff->zbuffer,0,size*sizeof(float));
 }
 
+//Check the Key Press
+
 int isKeydown(u_int x){
     return (GetAsyncKeyState(x)&0x8000) && (GetForegroundWindow()==hWnd);
 }
+
+
+//Key Bindings
+
 void keyCheck(){
 
             if(isKeydown('S'))
@@ -107,43 +164,71 @@ void keyCheck(){
             if(isKeydown(VK_UP))
                 vertical_offset-=10*deltatime;
 }
+
+
+//Main function call
+
 int main(){
+    
+    
+    //Get the cureent console buffer information
+
+    hWnd = GetForegroundWindow();
     CONSOLE_SCREEN_BUFFER_INFO info;
     GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE),&info);
+    
+    
+    //Set the screen buffer metadata
+
     wchar_t *screen;
     int swidth=info.dwSize.X;
     int sheight=info.dwSize.Y;
-
-    int cubeWidth=20;
     screen=(wchar_t*)malloc(sheight*swidth*sizeof(wchar_t));
     wmemset(screen,initial,swidth*sheight);
+
+    
+    //Set the size of the Cube buffer
+    
+    int cubeWidth=20;
     Buffer buff1;
     buff1.h=cubeWidth*2;
     buff1.w=cubeWidth*4;
     initBuffer(&buff1);
     resetBuffer(&buff1);
+    
+    //The normal vectors of the six of the cube faces
+    int normal[6][3]={0,0,1,1,0,0,-1,0,0,0,-1,0,0,0,-1,0,1,0};
+    
+    //Generate a new console buffer
 
     HANDLE console=CreateConsoleScreenBuffer(GENERIC_READ|GENERIC_WRITE,0,NULL,CONSOLE_TEXTMODE_BUFFER,NULL);
     DWORD words_written;
     COORD origin;
     origin.X=0;
     origin.Y=0;
+    
+    //Inrement for the coordinate calculation
     float increment=0.6;
+    
+    //Pause flag
     int pauseFlag=0;
-    const float radtoDEG=180/3.14159265359;
-    const  float Twopi=6.28318530718;
+
+    //Set up the system clock variables
     LARGE_INTEGER start,end,frequency;
     LONGLONG elapsedticks;
-    printf("\033[?25l");
-    int normal[6][3]={0,0,1,1,0,0,-1,0,0,0,-1,0,0,0,-1,0,1,0};
+
+    /* DWORD dw;
+    GetConsoleMode(GetStdHandle(STD_INPUT_HANDLE),&dw);
+    SetConsoleMode(GetStdHandle(STD_INPUT_HANDLE),~ENABLE_ECHO_INPUT); */
+    //swap the generated buffer with the standard buffer
     SetConsoleActiveScreenBuffer(console);
-    hWnd = GetForegroundWindow();
-    while(!((GetAsyncKeyState(VK_SPACE)&0x8000) && (GetForegroundWindow()==hWnd)))
+    
+    //Animation loop Start
+    while(!isKeydown(VK_SPACE))
     {
+        //Start the clock
         QueryPerformanceCounter(&start);
         QueryPerformanceFrequency(&frequency);
-        resetBuffer(&buff1);
-        wmemset(screen,initial,swidth*sheight);
 
         //Pre calculating sin and cos
         sa=sin(A);
@@ -152,6 +237,10 @@ int main(){
         ca=cos(A);
         cb=cos(B);
         cc=cos(C);
+        
+        
+        //Plotting the points in the cube buffer
+
         for(float cubeX=-cubeWidth;cubeX<cubeWidth;cubeX+=increment){
             for(float cubeY=-cubeWidth;cubeY<cubeWidth;cubeY+=increment){
                 calculateRotation(cubeX, cubeY, cubeWidth, &buff1,'@',normal[0]);
@@ -164,7 +253,10 @@ int main(){
             }
         }
         
-        if((GetAsyncKeyState('R')& 0x8000)){
+        
+        //Refreshing the window
+
+        if(isKeydown('R')){
             GetConsoleScreenBufferInfo(console,&info);
             swidth=info.dwSize.X;
             sheight=info.dwSize.Y;
@@ -175,12 +267,17 @@ int main(){
             horizontal_offset=0;
         }
 
+        //Writing the data to the screen buffer from the cube buffer
+
         for(int j=0,l=vertical_offset;  j<buff1.h && l<sheight; j++,l++){
             for(int k=0,m=horizontal_offset; k<buff1.w && m<swidth; k++,m++){
                 screen[l*swidth+m]=buff1.buffer[j*buff1.w+k];
             }
         } 
-        if(!((GetAsyncKeyState('P') & 0x8000)&& GetForegroundWindow()==hWnd) && !pauseFlag){
+
+        //Pause The animation
+
+        if(!isKeydown('P') && !pauseFlag){
             swprintf(screen,swidth*(sheight),L"[FPS : %.2f]",1/deltatime);
             A+=rotInc[0]*deltatime;
             B+=rotInc[1]*deltatime;
@@ -192,6 +289,8 @@ int main(){
             if(isKeydown('O'))
                 pauseFlag=0;
             
+            //Print the info in the pause mode
+
             swprintf(screen,swidth*sheight,L"[X Rotation : %.2f°,S-W]",A*radtoDEG);
             swprintf(screen+swidth,swidth*(sheight),L"[Y Rotation : %.2f°,A-D]",B*radtoDEG);
             swprintf(screen+swidth*2,swidth*(sheight),L"[Z Rotation : %.2f°,Q-E]",C*radtoDEG);
@@ -199,7 +298,9 @@ int main(){
             swprintf(screen+swidth*4,swidth*(sheight),L"[Distance from camera : %.2f,C-V]",distance);
             swprintf(screen+swidth*5,swidth*sheight,L"[FPS : %.2f]",1/deltatime);
         }
-        WriteConsoleOutputCharacterW(console,screen,swidth*sheight,origin,&words_written);
+        
+        //Keep the value of rotation angle within bounds
+
         if(A>Twopi)
             A-=Twopi;
         if(B>Twopi)
@@ -213,14 +314,29 @@ int main(){
         if(C<0)
             C=Twopi;
 
+        //Write the screen buffer to the console
+
+        WriteConsoleOutputCharacterW(console,screen,swidth*sheight,origin,&words_written);
+        
+        //resetting the buffers for the next frame
+        resetBuffer(&buff1);
+        wmemset(screen,initial,swidth*sheight);
+        
+        //delta time calculation
         QueryPerformanceCounter(&end);
         elapsedticks=end.QuadPart-start.QuadPart;
         deltatime=(double)(elapsedticks)/frequency.QuadPart;
 
     }
+
+    SetConsoleActiveScreenBuffer(GetStdHandle(STD_OUTPUT_HANDLE)); 
+    FlushConsoleInputBuffer(GetStdHandle(STD_INPUT_HANDLE));
+    //Free the console handle 
     CloseHandle(console);
-    SetConsoleActiveScreenBuffer(GetStdHandle(STD_OUTPUT_HANDLE));
-    printf("Screen Width : %d\nScreen Height: %d\n",swidth,sheight);
+    /* SetConsoleMode(GetStdHandle(STD_INPUT_HANDLE),dw); */
+    
+    //Print the last known resolution and FPS
+    printf("Screen Width : %d\nScreen Height: %d\nFPS :%.2f\n",swidth,sheight,1/deltatime);
     return 0;
 }
 
