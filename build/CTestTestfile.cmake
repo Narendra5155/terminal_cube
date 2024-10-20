0b6d43cfc11e861616fc4c04ee7c7c21ba45002a
Narendra5155 <hindwarnarendra@gmail.com>
2024-10-20 10:10:27 +0530

    Remove build folder

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/hindw/OneDrive/Desktop/Codespace/c-programming-2/New folder/ascii_art2
-# Build directory: C:/Users/hindw/OneDrive/Desktop/Codespace/c-programming-2/New folder/ascii_art2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
